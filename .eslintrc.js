769185ee5ad1857ac8fb6a48630a741515422ec0
Linus Groh <mail@linusgroh.de>
2019-12-16 20:24:15 +0000

    Set no-console/no-debugger to "warn" in dev mode

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,8 +5,8 @@ module.exports = {
   },
   extends: ["plugin:vue/essential", "@vue/prettier"],
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "warn",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "warn",
     "prettier/prettier": [
       "error",
       {
