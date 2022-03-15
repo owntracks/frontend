a386c15de10b1e159095a0ea3e896d24cba945e0
Linus Groh <mail@linusgroh.de>
2022-03-15 23:12:50 +0000

    Fix jest configuration

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,8 @@
 module.exports = {
+  testEnvironment: "jsdom",
   moduleFileExtensions: ["js", "jsx", "json", "vue"],
   transform: {
-    "^.+\\.vue$": "vue-jest",
+    "^.+\\.vue$": "@vue/vue2-jest",
     ".+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$":
       "jest-transform-stub",
     "^.+\\.jsx?$": "babel-jest",
