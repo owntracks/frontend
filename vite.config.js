998a97131bc37113f3a4c35c58f0151c872791a4
Linus Groh <mail@linusgroh.de>
2024-06-14 17:37:26 +0200

    Update vite config to emit relative paths
    Closes #118.

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import version from "vite-plugin-package-version";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "",
   plugins: [vue(), version()],
   resolve: {
     alias: {
